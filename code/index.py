fa91abb992817c1b2288df983182036b5ec793f6
yangrudan <1549029765@qq.com>
2024-04-11 17:29:01 +0800

    名字好听一点

--- code/index.py
+++ code/index.py
@@ -11,6 +11,30 @@ def index():
 def 企业简介():
     return "企业简介"
 
+@app.route("/下属企业")
+def 企业简介():
+    return "下属企业"
+
+@app.route("/产品中心")
+def 企业简介():
+    return "产品中心"
+
+@app.route("/主营业务")
+def 企业简介():
+    return "主营业务"
+
+@app.route("/企业文化")
+def 企业简介():
+    return "企业文化"
+
+@app.route("/企业招聘")
+def 企业简介():
+    return "企业招聘"
+
+@app.route("/联系我们")
+def 企业简介():
+    return "联系我们"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
