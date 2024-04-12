876dd958536ada747a2c7cc3d98148ee51d0365a
yangrudan <1549029765@qq.com>
2024-04-12 09:36:47 +0800

    暂时先这样

--- code/index.py
+++ code/index.py
@@ -11,29 +11,10 @@ def index():
 def 企业简介():
     return "企业简介"
 
-@app.route("/下属企业")
-def 企业简介():
-    return "下属企业"
+@app.route("/news")
+def news():
+    return "latest news"
 
-@app.route("/产品中心")
-def 企业简介():
-    return "产品中心"
-
-@app.route("/主营业务")
-def 企业简介():
-    return "主营业务"
-
-@app.route("/企业文化")
-def 企业简介():
-    return "企业文化"
-
-@app.route("/企业招聘")
-def 企业简介():
-    return "企业招聘"
-
-@app.route("/联系我们")
-def 企业简介():
-    return "联系我们"
 
 
 if __name__ == "__main__":
